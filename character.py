2fcbd3a0bd5afdb8c5846f4f28bf1c0b012bd0af
Daniel Walkusz <danwalkusz@gmail.com>
2020-06-02 17:27:59 +0200

    Daniel commit

--- character.py
+++ character.py
@@ -1,8 +1,10 @@
 import random
+from global_variables import *
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, parent):
+        self.parent = parent
         self.name = ''
         self.job = ''
         self.maxHP = 0
@@ -23,8 +25,6 @@ class Player:
 
     def fight(self, enemy):
         damage = self.STR
-        # damage -= self.maxDEF
-        # self.maxHP -= damage
         damage -= enemy.maxDEF
         enemy.maxHP -= damage
         if enemy.maxHP < 0:
@@ -47,9 +47,47 @@ class Player:
     def die(self):
         print("Jestem martwy. ~", self.name)
 
+    def move(self, dest=None):
+        if not dest:
+            ask = " Where would you like to move to?\n"
+            print(ask)
+            dest = input(" ")
+        none_move = ["0", "1", "2", "3", "4"]
+        if dest in ['up', 'west']:
+            if zonemap[self.location][UP] in none_move:
+                print(" !!! Here is a big wall !!! ")
+            else:
+                destination = zonemap[self.location][UP]
+                self.movement_handler(destination)
+        elif dest in ['left', 'west']:
+            if zonemap[self.location][LEFT] in none_move:
+                print(" !!! Here is a big wall !!! ")
+            else:
+                destination = zonemap[self.location][LEFT]
+                self.movement_handler(destination)
+        elif dest in ['right', 'east']:
+            if zonemap[self.location][RIGHT] in none_move:
+                print(" !!! Here is a big wall !!! ")
+            else:
+                destination = zonemap[self.location][RIGHT]
+                self.movement_handler(destination)
+        elif dest in ['down', 'south']:
+            if zonemap[self.location][DOWN] not in none_move:
+                print(" !!! Here is a big wall !!!")
+            else:
+                destination = zonemap[self.location][DOWN]
+                self.movement_handler(destination)
+
+    def movement_handler(self, destination):
+        self.parent.myEnemy.randomize_enemy()
+        print("\n" + " You have moved to the " + destination + ".")
+        self.location = destination
+        self.parent.location_print()
+
 
 class Enemy(object):
-    def __init__(self):
+    def __init__(self, parent):
+        self.parent = parent
         self.name = ''
         self.job = ''
         self.maxHP = 0
@@ -69,3 +107,44 @@ class Enemy(object):
 
     def die(self):
         print("Jestem martwy. ~", self.name)
+
+    def randomize_enemy(self):
+        i = int(random.randrange(1, 5))
+        if i == 1:
+            print(" You have not encountered anything dangerous on your way ")
+        elif i == 2:
+            self.name = 'robber'
+            self.job = 'robber'
+            self.maxHP = 100
+            self.maxMP = 20
+            self.maxDEF = 5
+            self.STR = 60
+            print(" On the way you met a robber")
+            self.parent.fight()
+        elif i == 3:
+            self.name = 'goblins'
+            self.job = 'robber'
+            self.maxHP = 70
+            self.maxMP = 0
+            self.maxDEF = 2
+            self.STR = 45
+            print(" On the way you met a goblins")
+            self.parent.fight()
+        elif i == 4:
+            self.name = 'traveling warrior'
+            self.job = 'warrior'
+            self.maxHP = 150
+            self.maxMP = 20
+            self.maxDEF = 10
+            self.STR = 65
+            print(" On the way you met a traveling warrior")
+            self.parent.fight()
+        elif i == 5:
+            self.name = 'aggressive wolf'
+            self.job = 'animal'
+            self.maxHP = 90
+            self.maxMP = 0
+            self.maxDEF = 0
+            self.STR = 55
+            print(" On the way you met an aggressive wolf")
+            self.parent.fight()
--- diologs.py
+++ diologs.py
@@ -1,29 +1,35 @@
 import sys
 import time
 
+
 class dialogs:
     def __init__(self):
         self.dialog = ""
+
     def dialog_print03(self):
         for character in self.dialog:
             sys.stdout.write(character)
             sys.stdout.flush()
             time.sleep(0.3)
+
     def dialog_print0025(self):
         for character in self.dialog:
             sys.stdout.write(character)
             sys.stdout.flush()
             time.sleep(0.025)
+
     def dialog_print01(self):
         for character in self.dialog:
             sys.stdout.write(character)
             sys.stdout.flush()
             time.sleep(0.1)
+
     def dialog_print001(self):
         for character in self.dialog:
             sys.stdout.write(character)
             sys.stdout.flush()
             time.sleep(0.01)
+
     def dialog_print005(self):
         for character in self.dialog:
             sys.stdout.write(character)
